d495cf074db11a591a692777f0d0d25ef52c9066
MichaelMGonzalez <mmg005@ucsd.edu>
2016-02-08 17:41:00 -0800

    slowed gummys

--- Assets/Scripts/ChangeGravity.cs
+++ Assets/Scripts/ChangeGravity.cs
@@ -4,14 +4,15 @@ using System.Collections;
 public class ChangeGravity : MonoBehaviour {
 
     public Vector3 direction = Vector3.down * 9.81f;
+    public float speed = 1f;
+    //public float 
 
     Vector3 randDirection = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
     void OnCollisionEnter(Collision collision)
     {
+            Physics.gravity = speed * randDirection.normalized;
         if(collision.gameObject.tag == "Player")
         {
-            Physics.gravity = 9.81f * randDirection.normalized;
-            Destroy(this.gameObject);
         }
         if(collision.gameObject.tag == "Wall")
         {
